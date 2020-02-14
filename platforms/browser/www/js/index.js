138ff822999c2344a1f16b9cdd67e713b07a63d1
Chris <mail.lamb@gmail.com>
2020-02-14 10:53:00 +0000

    1.4.1

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -153,33 +153,33 @@ function onDeviceReady() {
 }
 
 var admobid = {};
-	if (/(android)/i.test(navigator.userAgent)) {
-		admobid = {
-			// for Android
-			banner: 'ca-app-pub-8816517022745547/4602167137'
-			// interstitial: 'ca-app-pub-6869992474017983/1657046752'
-		};
-	} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-		admobid = {
-			// for iOS
-			banner: 'ca-app-pub-8816517022745547/4602167137'
-			// interstitial: 'ca-app-pub-6869992474017983/7563979554'
-		};
-	} else {
-		admobid = {
-			// for Windows Phone
-			banner: 'ca-app-pub-8816517022745547/4602167137'
-			// interstitial: 'ca-app-pub-6869992474017983/1355127956'
-		};
+if (/(android)/i.test(navigator.userAgent)) {
+	admobid = {
+		// for Android
+		banner: 'ca-app-pub-8816517022745547/4602167137'
+		// interstitial: 'ca-app-pub-6869992474017983/1657046752'
+	};
+} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+	admobid = {
+		// for iOS
+		banner: 'ca-app-pub-8816517022745547/4602167137'
+		// interstitial: 'ca-app-pub-6869992474017983/7563979554'
+	};
+} else {
+	admobid = {
+		// for Windows Phone
+		banner: 'ca-app-pub-8816517022745547/4602167137'
+		// interstitial: 'ca-app-pub-6869992474017983/1355127956'
+	};
+}
+
+function initApp() {
+	if (AdMob) {
+		AdMob.createBanner({
+			adId: admobid.banner,
+			position: AdMob.AD_POSITION.BOTTOM_CENTER,
+			autoShow: true
+		});
 	}
-	
-	function initApp() {
-		if (AdMob) {
-			AdMob.createBanner({
-				adId: admobid.banner,
-				position: AdMob.AD_POSITION.BOTTOM_CENTER,
-				autoShow: true
-			});
-		}
-	}
-	document.addEventListener('deviceready', initApp, false);
+}
+document.addEventListener('deviceready', initApp, false);
